8ed2f5a6ba850ae901f2dcc8a1929bb3c9ca6b7b
_tango <shurinskiy@mail.ru>
2021-08-07 22:30:25 +0600

    start styling

---
+++ src/blocks/header/header.js
@@ -0,0 +1,13 @@
+import 'owl.carousel';
+
+(() => {
+
+	$('.header__slider-items').owlCarousel({
+		loop: true,
+		items: 1,
+		margin: 0,
+		dots: false,
+		nav: false
+	});
+
+})();
---
+++ src/js/blocks.js
@@ -0,0 +1,6 @@
+import "../blocks/header/header.js";
+import "../blocks/articles/articles.js";
+import "../blocks/catalog/catalog.js";
+import "../blocks/culture/culture.js";
+import "../blocks/media/media.js";
+import "../blocks/posters/posters.js";
