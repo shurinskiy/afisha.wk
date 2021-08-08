7cbe32fa01e49bb6c2fe1c6cf292da4852edbbb9
_tango <shurinskiy@mail.ru>
2021-08-08 17:15:10 +0600

    articles block ready

--- src/blocks/articles/articles.js
+++ src/blocks/articles/articles.js
@@ -1,4 +1,27 @@
+import 'owl.carousel';
+
 (() => {
 
+	$('.articles__items').owlCarousel({
+		loop: true,
+		items: 3,
+		margin: 30,
+		dots: false,
+		nav: true,
+		navText: ['', ''],
+		navClass: ['articles__prev', 'articles__next'],
+		responsive: {
+			0: {
+				items: 1,
+			},
+			480: {
+				items: 2,
+			},
+			740: {
+				items: 3,
+			}
+		}
+
+	});
 
 })();
