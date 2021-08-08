3138f11ca1ef4d519e2b5003cc4c06dafaa7b2b0
_tango <shurinskiy@mail.ru>
2021-08-08 17:39:06 +0600

    posters block ready

--- src/blocks/posters/posters.js
+++ src/blocks/posters/posters.js
@@ -1,4 +1,28 @@
+import 'owl.carousel';
+
 (() => {
 
+	$('.posters__slider').owlCarousel({
+		loop: true,
+		items: 3,
+		margin: 30,
+		dotsEach: true,
+		dotClass: 'posters__dot',
+		nav: true,
+		navText: ['', ''],
+		navClass: ['posters__prev', 'posters__next'],
+		responsive: {
+			0: {
+				items: 1,
+			},
+			480: {
+				items: 2,
+			},
+			760: {
+				items: 3,
+			}
+		}
+
+	});
 
 })();
