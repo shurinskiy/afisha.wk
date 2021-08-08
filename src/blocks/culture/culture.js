8bb00e6576d39f4d848a585c3575dc7a5b892f33
_tango <shurinskiy@mail.ru>
2021-08-08 15:14:09 +0600

    culture slider

--- src/blocks/culture/culture.js
+++ src/blocks/culture/culture.js
@@ -1,4 +1,35 @@
+import 'owl.carousel';
+
 (() => {
+	let $side = $('.culture__side');
+
+	let insertion = function (e) {
+		if (!e.namespace) { return; }
+
+		let carousel = e.relatedTarget;
+		let $current = carousel.$element.find('.owl-item').eq(e.item.index);
+
+		$side
+			.find('.culture__info')
+			.hide()
+			.html($current.find('.culture__item-info').html())
+			.fadeIn();
+	}
+
+
+	$('.culture__slider').owlCarousel({
+		loop: true,
+		items: 1,
+		margin: 0,
+		dotsEach: true,
+		dotClass: 'culture__dot',
+		nav: true,
+		navText: ['', ''],
+		navClass: ['culture__prev', 'culture__next'],
+		onInitialized: insertion,
+		onChanged: insertion
+	});
+
 
 
 })();
